69b6cb9d91384337a7ab0074bfa1f0377a355594
Gabe Hart <flyingmessier@gmail.com>
2019-12-02 11:17:10 -0500

    Added FSM2 class, deconstructor for FSMState, lexer2.hpp

---
+++ lexer2.hpp
@@ -0,0 +1,24 @@
+/*
+lexer built using fsm2
+*/
+#include "fsm2.hpp"
+#include <string>
+#include <vector>
+
+#ifndef LEXER2_HEADER
+#define LEXER2_HEADER
+
+namespace cmpl {
+
+  class Lexer2 {
+  private:
+    /* data */
+
+  public:
+    Lexer2 (arguments);
+    virtual ~Lexer2 ();
+  };
+
+} /* cmpl */
+
+#endif
